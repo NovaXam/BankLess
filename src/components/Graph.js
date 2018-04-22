1f9e6b5de5649d7e5643473187f1d18e6765405e
mrs.leonenko <mrs.leonenko@gmail.com>
2018-04-22 12:49:37 -0400

    diagram

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import graph from '../assets/graphs.png';
 import '../style/graph.css';
-
+import {PieChart} from 'react-easy-chart';
+import { PortionList } from '../components/PortionsList';
+import {Legend} from 'react-easy-chart';
 
 
 const Graph = (props) => {
+    const dat = [];
+    console.log('data', props);
+    props.portions.map((elem, i) => {
+        const porID = `${Object.keys(elem)[0]}`;
+        return dat.push({key: elem.name, value: elem.balance})
+     });
+
     return (
         <div className="col col-xm-8 graphs"> 
-            <img src={graph} alt="graph" />
+            <div>
+                <h3></h3>
+            </div>
+            <PieChart 
+                  size={170} 
+                  innerHoleSize={70}
+                  data={dat} />
+            <Legend data={dat} dataId={'key'} horizontal/>
+                          
         </div>
     );
+
 };
 
 export default Graph;
+
+
